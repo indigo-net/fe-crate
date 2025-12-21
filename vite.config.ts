5dc0c19610a8323ef6d00ebed61acf431824225c
TransparentDeveloper <2younsin@gmail.com>
2025-12-21 15:35:03 +0900

    feat: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 // @ts-ignore
-import eslint from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    eslint()
-  ],
-})
+  plugins: [react(), tailwindcss(), eslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
