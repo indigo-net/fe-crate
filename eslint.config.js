377a2f00eef9d7b8258ba63fba64d0732288b6e4
TransparentDeveloper <2younsin@gmail.com>
2025-12-21 22:46:25 +0900

    feat: import 정렬 순서

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-config-prettier'
-import { defineConfig } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   {
@@ -13,15 +14,193 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      prettier
+      prettier,
     ],
+    plugins: {
+      import: importPlugin,
+    },
     rules: {
       semi: ['error', 'always'],
-      quotes: ['error', 'single']
+      quotes: ['error', 'single'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: './lib/**',
+              group: 'sibling',
+              position: 'before',
+            },
+            {
+              pattern: './model/**',
+              group: 'sibling',
+              position: 'before',
+            },
+            {
+              pattern: './store/**',
+              group: 'sibling',
+              position: 'before',
+            },
+            {
+              pattern: './ui/**',
+              group: 'sibling',
+              position: 'before',
+            },
+            // shared
+            {
+              pattern: '@/shared/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/shared/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/shared/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/shared/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+            // entities
+            {
+              pattern: '@/entities/**/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/entities/**/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/entities/**/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/entities/**/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+            // features
+            {
+              pattern: '@/features/**/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/features/**/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/features/**/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/features/**/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+            // widgets
+            {
+              pattern: '@/widgets/**/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/widgets/**/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/widgets/**/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/widgets/**/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+            // pages
+            {
+              pattern: '@/pages/**/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/pages/**/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/pages/**/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/pages/**/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+            // apps
+            {
+              pattern: '@/apps/**/lib/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/apps/**/model/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/apps/**/store/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/apps/**/ui/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-// @ts-ignore
-import eslint from 'vite-plugin-eslint';
 import path from 'path';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
