c192a317d8e2f54ddcc672e60f7003d13e027bb5
TransparentDeveloper <2younsin@gmail.com>
2025-12-28 17:24:47 +0900

    feat: react-router-dom 설치 및 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import KakaoRedirect from './pages/kakao-redirect';
 import LandingPage from './pages/landing';
+import NewFormPage from './pages/new-form';
 
 const App = () => {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/new-form" element={<NewFormPage />} />
+        <Route path="/kakao-authorize" element={<KakaoRedirect />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
