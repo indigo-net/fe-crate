4bca1d4aa758e8a06e0549b8a89221e05b757bb9
TransparentDeveloper <2younsin@gmail.com>
2026-01-18 22:26:59 +0900

    fix: 질문 타입 기본값 수정

--- src/entities/form/ui/SingleChoiceQuestionCard/index.tsx
+++ src/entities/form/ui/SingleChoiceQuestionCard/index.tsx
@@ -29,7 +29,7 @@ const SingleChoiceQuestionCard = (props: Props) => {
       <div className="w-full flex justify-between items-center">
         <div className="relative">
           <select
-            value={question?.getValue('type') ?? 'SHORT_TEXT'}
+            value={question?.getValue('type') ?? 'SINGLE_CHOICE'}
             className="cursor-pointer px-3 py-1.5 text-xs font-slim-semibold rounded-slim-lg bg-bg-subtle border border-transparent hover:border-border-default text-text-secondary transition-all outline-none"
             onChange={e =>
               onTypeChange?.(question.getValue('id'), e.target.value as FormQuestionType)
